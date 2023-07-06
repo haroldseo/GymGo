caffcd517782a1d8665ff4a721a491466ababfb2
Harold Seo <harold.seo.612@gmail.com>
2023-07-06 14:12:24 -0700

    Create Navbar Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Navbar from "@/components/Navbar/Navbar";
+
 function App() {
   return (
     <>
-      <div className="app">App</div>
+      <div className="app bg-gray-20">
+        <Navbar />
+      </div>
     </>
   );
 }
