6f0a59f90c0d7cb13583eb141904ceb3868d593b
Harold Seo <harold.seo.612@gmail.com>
2023-07-11 14:05:51 -0700

    Import AnchorLink

--- src/components/Navbar/Link.tsx
+++ src/components/Navbar/Link.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {};
 
 const Link = (props: Props) => {
-  return <div>Link</div>;
+  return <AnchorLink></AnchorLink>;
 };
 
 export default Link;
