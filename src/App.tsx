fe2f26efc5cf4fe5b8c578ff5bb4b8ae015fb00c
Harold Seo <harold.seo.612@gmail.com>
2023-07-12 18:42:08 -0700

    Update App Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Navbar from "@/components/Navbar/Navbar";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState("home");
+  const [selectedPage, setSelectedPage] = useState<string>("home");
   return (
     <>
       <div className="app bg-gray-20">
